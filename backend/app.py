669a4f159caa455e9554451cb7edff85a45bdb8d
Frenchie Chua <frenchiechua123@gmail.com>
2024-02-21 19:19:21 +0800

    Cleaning

--- backend/app.py
+++ backend/app.py
@@ -39,11 +39,11 @@ def get_image_size(file_path):
 # -------------------- #
 # Loading the pre-trained model
 def load_model():
-    json_file = open('Model.json', 'r')
+    json_file = open('models/Model.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
-    loaded_model.load_weights("weights/model_A_weights.h5")
+    loaded_model.load_weights("models/model_A_weights.h5")
     return loaded_model
 
 # Function Description #
