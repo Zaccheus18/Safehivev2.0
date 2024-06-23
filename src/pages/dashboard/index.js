be6f391138242ff74adf185d8d5e115ff74b1f4e
Frenchie Chua <86643869+Zaccheus18@users.noreply.github.com>
2024-06-23 10:54:18 +0800

    Delete files

--- src/pages/dashboard/index.js
+++
@@ -1,8 +0,0 @@
-export * from "@/pages/dashboard/home";
-export * from "@/pages/dashboard/about";
-export * from "@/pages/dashboard/contact";
-export * from "@/pages/dashboard/travelogue";
-export * from "@/pages/dashboard/inspired_works";
-export * from "@/pages/dashboard/navigationbar";
-export * from "@/pages/dashboard/footer";
-
--- src/widgets/cards/index.js
+++
@@ -1,4 +0,0 @@
-export * from "@/widgets/cards/statistics-card";
-export { default as DragDropFile } from "@/widgets/cards/CrowdDetection";
-export { default as Results } from "@/widgets/cards/results";
-
